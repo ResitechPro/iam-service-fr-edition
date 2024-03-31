67e69eebac12bd76d7a1bec9724e168e3816ffe3
Issam Mezgueldi <issammez44@gmail.com>
2024-03-31 16:27:18 +0000

    start the microservice server

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('TCP_PORT'),
+    },
+  });
+
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
   });
+
   app.setGlobalPrefix('api');
-  await app.listen(3002);
+
+  await app.startAllMicroservices();
+
+  await app.listen(configService.get('HTTP_PORT'));
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import {
 import { UsersService } from './users.service';
 import { users } from 'src/drizzle/schema';
 import { InferInsertModel } from 'drizzle-orm';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 
 @Controller('users')
 export class UsersController {
@@ -40,6 +41,34 @@ export class UsersController {
 
   @Delete(':id')
   remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+
+  @EventPattern('create_user')
+  createMS(@Body() createUserDto: InferInsertModel<typeof users>) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @MessagePattern({ cmd: 'get_users' })
+  findAllMS() {
+    return this.usersService.findAll();
+  }
+
+  @MessagePattern({ cmd: 'get_user' })
+  findOneMS(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @EventPattern('update_user')
+  updateMS(
+    @Param('id') id: string,
+    @Body() updateUserDto: Partial<InferInsertModel<typeof users>>,
+  ) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @EventPattern('delete_user')
+  removeMS(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
 }
