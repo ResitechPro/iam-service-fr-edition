aa9f971bc875e1e0fe3280149e246d1f62afbe6f
Issam Mezgueldi <issammez44@gmail.com>
2024-03-31 17:21:46 +0000

    convert the permissions controller from http-based endpoints to message-based comunication method

--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -10,36 +10,34 @@ import {
 import { PermissionsService } from './permissions.service';
 import { permissions } from 'src/drizzle/schema';
 import { InferInsertModel } from 'drizzle-orm';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('permissions')
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
-  @Post()
+  @MessagePattern({ cmd: 'create_permission' })
   create(@Body() createPermissionDto: InferInsertModel<typeof permissions>) {
     return this.permissionsService.create(createPermissionDto);
   }
 
-  @Get()
+  @MessagePattern({ cmd: 'get_permissions' })
   findAll() {
     return this.permissionsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @MessagePattern({ cmd: 'get_permission' })
+  findOne(id: string) {
     return this.permissionsService.findOne(id);
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updatePermissionDto: Partial<InferInsertModel<typeof permissions>>,
-  ) {
-    return this.permissionsService.update(id, updatePermissionDto);
+  @MessagePattern({ cmd: 'update_permission' })
+  update({ id, ...rest }: Partial<InferInsertModel<typeof permissions>>) {
+    return this.permissionsService.update(id, rest);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @MessagePattern({ cmd: 'delete_permission' })
+  remove(id: string) {
     return this.permissionsService.remove(id);
   }
 }
